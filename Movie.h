b0bf07e84f078e9a166660ecfefe8974ed7d5fb1
David Lowe <doctorjlowe@verizon.net>
2015-05-10 11:27:17 -0700

    remove double qualifications

--- Movie.h
+++ Movie.h
@@ -62,7 +62,7 @@ public:
 		cout << "Date Viewed: " << viewed;
 	}
 
-	bool Movie::searchTarget(string target) {
+	bool searchTarget(string target) {
 		string ser = to_string(seriesNum);
 		if (getRuntimeMinutes().find(target) != string::npos // search string of runtimeMinutes
 			|| name.find(target) != string::npos // search name
